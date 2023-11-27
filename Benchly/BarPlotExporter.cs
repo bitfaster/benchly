0a5801fceb093434377f5b6ca7621be69fee33d4
Alex Peck <alexpeck@microsoft.com>
2023-11-27 14:38:27 -0800

    fix y axis

--- Benchly/BarPlotExporter.cs
+++ Benchly/BarPlotExporter.cs
@@ -4,6 +4,8 @@ using Plotly.NET.ImageExport;
 using BenchmarkDotNet.Loggers;
 using Plotly.NET;
 using Plotly.NET.LayoutObjects;
+using static Plotly.NET.StyleParam;
+using Microsoft.FSharp.Core;
 
 namespace Benchly
 {
@@ -81,7 +83,7 @@ namespace Benchly
 
             var byParam = summary.Reports.Select(r => new
             {
-                param = r.BenchmarkCase.Parameters[0].Value,
+                param = r.BenchmarkCase.Parameters.PrintInfo,
                 job = r.BenchmarkCase.Job.ResolvedId,
                 name = r.BenchmarkCase.Descriptor.WorkloadMethodDisplayInfo,
                 mean = r.Success ? ConvertNanosToMs(r.ResultStatistics.Mean) : 0
@@ -103,13 +105,14 @@ namespace Benchly
             }
 
             // https://github.com/plotly/Plotly.NET/issues/387
+            // The alignment of this isn't 100% correct. Another approach may be to give each sub chart an x axis title
             double xWidth = 1.0d / byParam.Count();
             double xMidpoint = xWidth / 2.0d;
             double[] xs = byParam.Select((_, index) => xMidpoint + (xWidth * index)).ToArray();
 
             var annotations = byParam.Select((p, index) => Annotation.init<double, double, string, string, string, string, string, string, string, string>(
                 X: xs[index],
-                Y: 1,
+                Y: -0.1,
                 XAnchor: StyleParam.XAnchorPosition.Center,
                 ShowArrow: false,
                 YAnchor: StyleParam.YAnchorPosition.Bottom,
@@ -118,8 +121,11 @@ namespace Benchly
                 YRef: "paper"
             ));
 
+            // this couples all the charts on the same row to have the same y axis
+            var pattern = new FSharpOption<LayoutGridPattern>(LayoutGridPattern.Coupled);
+
             Chart
-                .Grid<IEnumerable<GenericChart.GenericChart>>(1, byParam.Count()).Invoke(gridCharts)
+                .Grid<IEnumerable<GenericChart.GenericChart>>(1, byParam.Count(), Pattern: pattern).Invoke(gridCharts)
                 .WithAnnotations(annotations)
                 .WithoutVerticalGridlines()
                 .WithLayout(title)
