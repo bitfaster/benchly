f20ff81e1cdb747c4cde8aa6173883028d600665
Alex Peck <amwpeck@gmail.com>
2024-11-16 16:37:04 -0800

    update to plotly v5.0 (#31)
    * update
    
    * fix compile
    
    * short run
    
    * update dependencies
    
    ---------

--- Benchly/PlotExtensions.cs
+++ Benchly/PlotExtensions.cs
@@ -7,14 +7,14 @@ namespace Benchly
 {
     internal static class PlotExtensions
     {
-        public static GenericChart.GenericChart WithAxisTitles(this GenericChart.GenericChart chart, string yTitle)
+        public static GenericChart WithAxisTitles(this GenericChart chart, string yTitle)
         {
             var font = new FSharpOption<Font>(Font.init(Size: new FSharpOption<double>(16)));
             FSharpOption<string> yt = new FSharpOption<string>(yTitle);
             return chart.WithXAxisStyle(Title.init(Font: font)).WithYAxisStyle(Title.init(yt, Font: font));
         }
 
-        public static GenericChart.GenericChart WithAxisTitles(this GenericChart.GenericChart chart, string xTitle, string yTitle)
+        public static GenericChart WithAxisTitles(this GenericChart chart, string xTitle, string yTitle)
         {
             var font = new FSharpOption<Font>(Font.init(Size: new FSharpOption<double>(16)));
             FSharpOption<string> xt = new FSharpOption<string>(xTitle);
@@ -22,18 +22,18 @@ namespace Benchly
             return chart.WithXAxisStyle(Title.init(xt, Font: font)).WithYAxisStyle(Title.init(yt, Font: font));
         }
 
-        public static GenericChart.GenericChart WithoutVerticalGridlines(this GenericChart.GenericChart chart)
+        public static GenericChart WithoutVerticalGridlines(this GenericChart chart)
         {
             var gridColor = new FSharpOption<Color>(Color.fromKeyword(ColorKeyword.Gainsboro));
-            var yaxis = LinearAxis.init<IConvertible, IConvertible, IConvertible, IConvertible, IConvertible, IConvertible>(
+            var yaxis = LinearAxis.init<IConvertible, IConvertible, IConvertible, IConvertible, IConvertible, IConvertible, IConvertible, IConvertible>(
                 GridColor: gridColor,
                 ZeroLineColor: gridColor);
 
-            var axis = LinearAxis.init<IConvertible, IConvertible, IConvertible, IConvertible, IConvertible, IConvertible>(ShowGrid: new FSharpOption<bool>(false));
+            var axis = LinearAxis.init<IConvertible, IConvertible, IConvertible, IConvertible, IConvertible, IConvertible, IConvertible, IConvertible>(ShowGrid: new FSharpOption<bool>(false));
             return chart.WithXAxis(axis).WithYAxis(yaxis);
         }
 
-        public static GenericChart.GenericChart WithLayout(this GenericChart.GenericChart chart, string title)
+        public static GenericChart WithLayout(this GenericChart chart, string title)
         {
             var font = new FSharpOption<Font>(Font.init(Size: new FSharpOption<double>(24)));
             FSharpOption<Title> t = Title.init(Text: title, X: 0.5, Font: font);
@@ -42,13 +42,13 @@ namespace Benchly
             return chart.WithLayout(layout);
         }
 
-        public static GenericChart.GenericChart WithGroupBox(this GenericChart.GenericChart chart)
+        public static GenericChart WithGroupBox(this GenericChart chart)
         {
             Layout layout = Layout.init<IConvertible>(BoxMode: BoxMode.Group);
             return chart.WithLayout(layout);
         }
 
-        public static GenericChart.GenericChart WithLegendGroup(this GenericChart.GenericChart chart, string groupName, bool showLegend)
+        public static GenericChart WithLegendGroup(this GenericChart chart, string groupName, bool showLegend)
         {
             return chart.WithTraceInfo(LegendGroup: new FSharpOption<string>(groupName), ShowLegend: new FSharpOption<bool>(showLegend));
         }
